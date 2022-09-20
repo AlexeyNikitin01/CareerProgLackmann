1d9eb99ac25888e22d8e2daff52bec05acf55435
ThinkPadE470 <lyosha280761@gmail.com>
2022-09-20 19:35:26 +0300

    '1_1'

--- глава 9 1 массивы и строки/1_1.py
+++ глава 9 1 массивы и строки/1_1.py
@@ -4,58 +4,59 @@
 структур данных?
 """
 
-from random import choice
 
-
-def q_sort_str(word: str) -> str:
-    if len(word) < 2:
-        return word
-
-    random_letter = choice(word)
-
-    left = [j for j in word if ord(j) < ord(random_letter)]
-    right = [j for j in word if ord(j) > ord(random_letter)]
-    pivots = [j for j in word if ord(j) == ord(random_letter)]
-
-    sort_word = q_sort_str(''.join(left)) + ''.join(pivots) + q_sort_str(''.join(right))
-
-    return sort_word
-
-
+# O(n)
 def is_duplicate_letter(word: str) -> bool:
     if len(word) < 2:
         return False
-    """
-    for i in word:
-        count = 0
-        for j in word:
-            if i == j:
-                count += 1
-        if count > 1:
-            return False
-    """
-    """
-    for i in range(len(word)):
-        if word[i] in word[:i] + word[i+1:]:
-            return False
-    """
-    """
+
     check_letter = {}
     for i in word:
         if check_letter.get(i):
             return False
         else:
             check_letter[i] = True
-    """
-    word = q_sort_str(word)
+    return True
 
+
+# O(nlogn)
+def is_duplicate_letter_2(word: str) -> bool:
+    if len(word) < 2:
+        return False
+    sorted_word = ''.join(sorted([i for i in word]))
     for i in range(1, len(word)):
-        if word[i] == word[i - 1]:
+        if sorted_word[i] == sorted_word[i-1]:
             return False
+    return True
 
+
+# O(n^2)
+def is_duplicate_letter_3(word: str) -> bool:
+    if len(word) < 2:
+        return False
+
+    for i in range(len(word)):
+        if word[i] in word[:i] + word[i+1:]:
+            return False
+    return True
+
+
+# O(n)
+def is_duplicate_letter_4(word: str) -> bool:
+    if len(word) < 2:
+        return False
+
+    if len(set(word)) == len(word):
+        return False
     return True
 
 
 if __name__ == '__main__':
     assert is_duplicate_letter('asdf') is True
     assert is_duplicate_letter('aaaf') is False
+    assert is_duplicate_letter_2('asdf') is True
+    assert is_duplicate_letter_2('aaaf') is False
+    assert is_duplicate_letter_3('asdf') is True
+    assert is_duplicate_letter_3('aaaf') is False
+    assert is_duplicate_letter_4('asdf') is True
+    assert is_duplicate_letter_4('aaaf') is False
